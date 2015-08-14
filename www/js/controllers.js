f7e6ac07cd79db3e3019e4ec1a968021b0e96fef
stamay <falco103-@hotmail.com>
2015-08-14 00:40:49 -0500

    ionic app
    Sunshine app con ionic framework

---
+++ www/js/controllers.js
@@ -0,0 +1,34 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats, $http) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+$scope.items= [];
+$http.get("http://api.openweathermap.org/data/2.5/forecast/?cnt=5&id=3531673&units=metric").then(function(response){
+$scope.items= response.data.list;
+console.log(response);
+
+});
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
